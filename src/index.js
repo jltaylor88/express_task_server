6a217838e22dd7d0cd4810f5ed11c31ace03a64d
Jason Taylor <jasonleetaylor88@gmail.com>
2023-11-03 19:18:28 +0000

    feat: setup the route for retrieving all users

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
-const { connectToDB } = require("./db/setup");
+const express = require("express");
+const User = require("./models/user");
 
-const main = async () => {
-	await connectToDB();
-};
+require("./db/setup").connectToDB();
 
-main();
+// Initialise the Express application and configure it to parse JSON in the request body
+const app = express();
+app.use(express.json());
+
+// The get route for retrieving all users
+app.get("/users", async (_req, res) => {
+	try {
+		const result = await User.find({});
+		res.send(result);
+	} catch (error) {
+		res.status(400).send(error);
+	}
+});
+
+// Listen for requests on port 3000
+app.listen(3000, () => {
+	console.log("Server is up on port 3000");
+});
